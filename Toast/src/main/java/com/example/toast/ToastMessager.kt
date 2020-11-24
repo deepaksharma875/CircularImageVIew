297cbffb01428c143ae92eee97736b58219c27f0
deepaksharma875 <shayer335@gmail.com>
2020-11-24 15:57:40 +0530

    message

---
+++ Toast/src/main/java/com/example/toast/ToastMessager.kt
@@ -0,0 +1,14 @@
+package com.example.toast
+
+import android.content.Context
+import android.widget.Toast
+
+
+
+
+class ToastMessager {
+
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
